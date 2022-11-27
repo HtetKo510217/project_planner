7e59aac93b113113f20267f28c21ee99cfbab8eb
htetko <htetko.connetis@gmail.com>
2022-11-27 20:20:15 +0630

    Project Planner Start Up and Fatch Api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <h1>home page</h1>
+   <div v-for="project in projects" :key="project.id">
+      <div>
+        <h3>{{project.title}}</h3>
+        <p>{{project.detail}}</p>
+      </div>
+   </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      projects:[],
+    }
+  },
+  mounted () {
+    fetch('http://localhost:3000/projects')
+    .then((response)=>{
+      return response.json()
+    })
+    .then((datas)=> {
+      this.projects = datas
+    })
+    .catch(()=>{
+
+    })
   }
 }
 </script>
