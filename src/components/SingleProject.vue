fea5ec65c3c13c6015322ea35c3aee6ff4b1e3ac
htetko <htetko.connetis@gmail.com>
2022-11-27 22:13:49 +0630

    adding material icon and css design

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="project" @click="showDetail = !showDetail">
-    <h3>{{project.title}}</h3>
+  <div class="project">
+    <div class="col">
+        <h3 @click="showDetail = !showDetail">{{project.title}}</h3>
+        <div>
+            <span class="material-icons">
+                delete
+            </span>
+            <span class="material-icons">
+                edit
+            </span>
+            <span class="material-icons">
+                done
+            </span>
+        </div>
+    </div>
     <p v-if="showDetail">{{project.detail}}</p>
   </div>
 </template>
@@ -20,12 +33,23 @@ export default {
     .project {
         padding: 15px;
         margin: 10px;
-        cursor: pointer;
         background: #f2f2f2;
-
+        border-left: 4px solid red;
+        border-radius: 5px;
     }
     .project h3 {
         color: indigo;
-        
+        cursor: pointer;
+        padding: 5px;
+    }
+    .col {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .col span {
+        margin-left: 10px;
+        padding: 4px;
+        cursor: pointer;
     }
 </style>
